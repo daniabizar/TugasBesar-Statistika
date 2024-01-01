a1fcdc032687f664209e8d75cb7bd89737901c7a
Dani Abizar Ahmad <90661449+daniabizar@users.noreply.github.com>
2024-01-02 02:46:40 +0700

    Add files via upload

---
+++ scriptR.R
@@ -0,0 +1,6 @@
+plot(tubesHotel$`Harga Hotel (Jutaan Dollar)`, tubesHotel$`Luas Area Hotel (Meter Kuadrat)`,
+     xlab = "Luas Area Hotel (Meter Kuadrat)",
+     ylab = "Harga Hotel (Jutaan Dollar)",
+     main = "Harga Hotel Berdasarkan Luas Area Hotel",
+     col = "blue"
+     )
